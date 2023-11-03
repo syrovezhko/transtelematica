6148db295bc4a6397c97348a9bb74c120e583afd
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-11-03 20:03:30 +0300

    feat: subCategories action types

--- src/store/itemReducer.ts
+++ src/store/itemReducer.ts
@@ -11,6 +11,42 @@ interface CategoryState {
   open: boolean,
 }
 
+export enum SubCategoryActionTypes {
+  GET_SUB_CATEGORIES     = 'GET_SUB_CATEGORIES',
+  SET_SUB_CATEGORIES     = 'SET_SUB_CATEGORIES',
+  DELETE_SUB_CATEGORY    = 'DELETE_SUB_CATEGORY',
+  HIGHLIGHT_SUB_CATEGORY = 'HIGHLIGHT_SUB_CATEGORY',
+  TOGGLE_SUB_CATEGORY    = 'TOGGLE_SUB_CATEGORY',
+}
+
+interface GetSubCategoriesAction {
+  type: SubCategoryActionTypes.GET_SUB_CATEGORIES;
+  payload: Category[]
+}
+interface SetSubCategoriesAction {
+  type: SubCategoryActionTypes.SET_SUB_CATEGORIES;
+  payload: Category
+}
+interface DeleteSubCategoryAction {
+  type: SubCategoryActionTypes.DELETE_SUB_CATEGORY;
+  payload: Category
+}
+interface HighlightSubCategoryAction {
+  type: SubCategoryActionTypes.HIGHLIGHT_SUB_CATEGORY;
+  payload: number
+}
+interface ToggleSubCategoryAction {
+  type: SubCategoryActionTypes.TOGGLE_SUB_CATEGORY;
+  payload: Category
+}
+
+export type SubCategoryAction =
+  GetSubCategoriesAction     |
+  SetSubCategoriesAction     |
+  DeleteSubCategoryAction    |
+  HighlightSubCategoryAction |
+  ToggleSubCategoryAction
+
 const initialState: CategoryState = {
   categories: [],
   category: [],
