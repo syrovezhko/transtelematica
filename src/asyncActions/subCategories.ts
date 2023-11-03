d2ecdf334cfb711168916f7923fc7dc702cf39fd
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-11-03 20:09:12 +0300

    feat: async get subCategories action

---
+++ src/asyncActions/subCategories.ts
@@ -0,0 +1,24 @@
+import { getSubCategoriesAction, SubCategoryAction } from "./../store/itemReducer";
+import { Dispatch } from "../../node_modules/redux/index";
+import subCategories from './../data/subCategories.json';
+
+const emulateAPI = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(subCategories), 0);
+});
+
+export const fetchSubCategories = () => {
+  return async (dispatch: Dispatch<SubCategoryAction>) => {
+    const data = await emulateAPI();
+    dispatch(getSubCategoriesAction(data));
+  };
+};
+
+// TODO: rewrite for real server
+// export const fetchSubCategories = () => {
+//   return dispatch => {
+//     fetch('URL')
+//       .then(response => response.json())
+//       .then(json => dispatch(getSubCategoriesAction(json)))
+//   }
+// }
