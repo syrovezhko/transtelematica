562da953fa930123be88730210e60088424fd048
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-11-03 16:28:28 +0300

    feat: CategoryActionTypes enumeration

--- src/store/categoryReducer.ts
+++ src/store/categoryReducer.ts
@@ -10,6 +10,14 @@ interface CategoryState {
   highlight: number,
   open: boolean,
 }
+
+export enum CategoryActionTypes {
+  GET_CATEGORIES     = 'GET_CATEGORIES',
+  SET_CATEGORIES     = 'SET_CATEGORIES',
+  DELETE_CATEGORY    = 'DELETE_CATEGORY',
+  HIGHLIGHT_CATEGORY = 'HIGHLIGHT_CATEGORY',
+  TOGGLE_CATEGORY    = 'TOGGLE_CATEGORY',
+}
 const initialState: CategoryState = {
   categories: [],
   category: undefined,
