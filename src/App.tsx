37d7f7be499a69545e94dce5300645042c30bf85
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-11-03 16:45:19 +0300

    feat: fetchCategories call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './app.css';
+import { fetchCategories } from './asyncActions/categories';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, []);
+
   return (
     <section className="wrapper">
     </section>
