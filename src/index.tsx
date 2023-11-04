486a4a08e0afdf9333b9e0e8aed2f9feec6b2bec
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-11-04 17:44:21 +0300

    refactor: renamed app.css to index.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
+import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
