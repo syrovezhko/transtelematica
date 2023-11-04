737163a03e207f76b4d7fb1fd06d376443c01e77
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-11-04 19:35:30 +0300

    refactor: apply HOC

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Container from './UI/Container/Container';
+import HoC from './hoc/hoc';
 
 const App = () => {
   return <Container />;
 };
 
-export default App;
+export default HoC(App);
