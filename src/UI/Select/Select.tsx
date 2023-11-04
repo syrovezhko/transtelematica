5400b7a6d81cb605074787f0b1795c5db01655c7
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-11-04 19:26:45 +0300

    refactor: made reduxDeleteOne as optional

--- src/UI/Select/Select.tsx
+++ src/UI/Select/Select.tsx
@@ -1,7 +1,13 @@
 import { Dispatch, useEffect, useRef, useState } from 'react';
 import './select.css';
 import React from 'react';
-import { CategoryAction, CategoryState, Category, SubCategoryState, SubCategoryAction } from './../../types/types';
+import {
+  CategoryAction,
+  CategoryState,
+  Category,
+  SubCategoryState,
+  SubCategoryAction
+} from './../../types/types';
 
 type SelectProps = {
   multiple: boolean;
@@ -11,7 +17,7 @@ type SelectProps = {
   reduxHighlight: Dispatch<CategoryAction> & Dispatch<number>;
   reduxToggle: Dispatch<CategoryAction> & Dispatch<boolean>;
   parent: number | null;
-  reduxDeleteOne: Dispatch<SubCategoryAction> & Dispatch<Category>;
+  reduxDeleteOne?: Dispatch<SubCategoryAction> & Dispatch<Category>;
 };
 
 export function Select({
@@ -29,8 +35,8 @@ export function Select({
   const containerRef = useRef<HTMLDivElement>(null);
   const value = reduxCategory.category as Category;
   const valueMultiple = reduxCategory.categories.filter((i) => i.parent_id == parent) as Category[];
-  const multipleCategory = reduxCategory.category as Category[]
-  const singleCategory = reduxCategory.category as Category
+  const multipleCategory = reduxCategory.category as Category[];
+  const singleCategory = reduxCategory.category as Category;
 
   useEffect(() => {
     reduxToggle(isOpen);
@@ -105,7 +111,7 @@ export function Select({
                 key={v.id}
                 onClick={(e) => {
                   e.stopPropagation();
-                  reduxDeleteOne(v);
+                  if (reduxDeleteOne) reduxDeleteOne(v);
                 }}
                 className="option-badge">
                 {v.name}
@@ -117,7 +123,7 @@ export function Select({
       <button
         onClick={(e) => {
           e.stopPropagation();
-          console.log(e)
+          console.log(e);
           reduxDelete('e');
         }}
         className="clear-btn">
